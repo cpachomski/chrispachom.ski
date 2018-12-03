9d1e9ab3444b679d95ce0eb2e12f284f69579320
Chris Pachomski <cpachomski@gmail.com>
2018-12-03 15:06:49 -0500

    add pre test options

--- .storybook/storyshots.test.js
+++ .storybook/storyshots.test.js
@@ -3,10 +3,26 @@ import registerRequireContextHook from 'babel-plugin-require-context-hook/regist
 import initStoryshots from '@storybook/addon-storyshots'
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer'
 
+const getMatchOptions = ({ context: { kind, story }, url }) => {
+  return {
+    failureThreshold: 0.2,
+    failureThresholdType: 'percent',
+  }
+}
+const beforeScreenshot = (page, { context: { kind, story }, url }) => {
+  return new Promise(resolve =>
+    setTimeout(() => {
+      resolve()
+    }, 600)
+  )
+}
+
 registerRequireContextHook()
 initStoryshots({
   suite: 'Image storyshots',
   test: imageSnapshot({
     storybookUrl: `file://${path.resolve(__dirname, '.out')}`,
+    getMatchOptions,
+    beforeScreenshot,
   }),
 })
