40a4f897b304266cc80836c0dd86e17abba63a49
Chris Pachomski <cpachomski@gmail.com>
2018-12-03 14:47:57 -0500

    try again

--- src/components/vertical-text/styled.js
+++ src/components/vertical-text/styled.js
@@ -2,6 +2,6 @@ import styled from 'styled-components'
 
 export const Character = styled.div`
   font-size: 28px;
-  font-family: Arial, Helvetica, sans-serif;
+  font-family: Montserrat;
   text-transform: uppercase;
 `
