80725f54dedf1e8e7e8ff8911d0996d4b1337870
Chris Pachomski <cpachomski@gmail.com>
2018-12-05 00:05:58 -0500

    change font styles of vertical text

--- src/components/vertical-text/styled.js
+++ src/components/vertical-text/styled.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 export const Character = styled.div`
   font-size: 28px;
+  font-weight: bold;
+  line-height: 1.2;
   font-family: Montserrat;
   text-transform: uppercase;
 `
