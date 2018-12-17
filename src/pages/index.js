7fdb295a9abdb86823849fb23c28c8ff41dd81f1
Chris Pachomski <chris@Chriss-MacBook-Pro.local>
2018-12-16 21:34:28 -0500

    update sunken platform to raise via settimeout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,35 @@
 import './fonts.css'
-import React from 'react'
+
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import SinkingPlatform from '../components/sinking-platform'
 import Header from '../components/header'
 
 const Page = styled.div`
   display: flex;
-  margin-top: 50px;
+  margin: 50px 0px;
   justify-content: center;
   align-items: center;
 `
 
-const IndexPage = () => {
-  return (
-    <Page>
-      <SinkingPlatform isSunken={false}>
-        <Header />
-      </SinkingPlatform>
-    </Page>
-  )
+class IndexPage extends Component {
+  state = {
+    platformIsSunken: true,
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ platformIsSunken: false }), 500)
+  }
+
+  render() {
+    return (
+      <Page>
+        <SinkingPlatform isSunken={this.state.platformIsSunken}>
+          <Header />
+        </SinkingPlatform>
+      </Page>
+    )
+  }
 }
 
 export default IndexPage
