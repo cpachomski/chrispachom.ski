319921f5ee3de54f7b6e0ccde3e05a337a18a7c5
Chris Pachomski <cpachomski@gmail.com>
2019-01-20 17:02:45 -0500

    update gatsby dependencies and remove package-lock in favor of yarn.lock

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import './reset.css'
 
 import React, { Component } from 'react'
 import { Router } from '@reach/router'
-import { Grid, Flex, Content } from './styled'
+import styled from 'styled-components'
 import Link from '../components/link'
 import ComputerDesktop from '../components/computer-desktop'
 import Amplifier from '../components/amplifier'
@@ -11,6 +11,60 @@ import Header from '../components/header'
 import MeAtWork from '../components/me-at-work'
 import VerticalText from '../components/vertical-text'
 import Section from '../section'
+
+const Content = styled.div`
+  height: 100%;
+  width: 100%;
+
+  > div {
+    height: 100%;
+    width: 100%;
+  }
+`
+
+const Flex = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const Grid = styled.div`
+  margin: 0 auto;
+  max-width: 1400px;
+  width: 100%;
+  height: 100vh;
+  padding: 30px;
+  box-sizing: border-box;
+
+  display: grid;
+  grid-template-columns: 100px 1fr 250px;
+  grid-template-rows: repeat(1, 1fr);
+  grid-column-gap: 25px;
+  align-items: center;
+  justify-items: center;
+
+  > * {
+    height: 100%;
+    width: 100%;
+  }
+
+  > *:first-child {
+    grid-column: 1 / 2;
+    grid-row: 1 / 4;
+  }
+
+  > *:nth-child(2) {
+    grid-column: 2 / 3;
+    grid-row: 1;
+  }
+
+  > *:nth-child(3) {
+    grid-column: 3;
+    grid-row: 1 / 3;
+    align-self: start;
+  }
+`
 
 const Layout = ({ children }) => (
   <Grid>
