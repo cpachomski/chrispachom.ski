ea8f119774d600d201bf0810451d1b447b0a2455
Chris Pachomski <cpachomski@gmail.com>
2019-01-19 19:53:18 -0500

    add pass through props for iconSize

--- src/components/icon-menu/index.js
+++ src/components/icon-menu/index.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Icon from '../icon'
 import { Column } from './styled'
 
-const IconMenu = () => (
+const IconMenu = ({ iconSize = 35 }) => (
   <Column>
-    <Icon size={30} company="github" url="https://www.github.com/cpachomski/" />
     <Icon
-      size={30}
+      size={iconSize}
+      company="github"
+      url="https://www.github.com/cpachomski/"
+    />
+    <Icon
+      size={iconSize}
       company="linkedin"
       url="https://www.linkedin.com/in/chrispachomski/"
     />
   </Column>
 )
+
+IconMenu.propTypes = {
+  iconSize: PropTypes.number,
+}
 
 export default IconMenu
