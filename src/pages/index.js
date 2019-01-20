bc41f6d30d93d64d07d0ae7431408a76a9ffc187
Chris Pachomski <cpachomski@gmail.com>
2019-01-20 14:54:12 -0500

    add hover state machine

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,28 +19,41 @@ const Button = styled.button`
   text-align: center;
   background: transparent;
   border: none;
+  width: 100%;
+  height: 100%;
+  max-height: 250px;
+  max-width: 250px;
+  padding-right: 25px;
   outline: none;
-  height: 200px;
-  width: 200px;
   display: flex;
-  align-items: flex-end;
+  align-items: center;
+  justify-self: end;
+  border: 4px solid transparent;
+  transition: border-color;
 
   > *:first-child {
     margin-right: 25px;
   }
+
+  &:hover {
+    cursor: pointer;
+    border-color: black;
+  }
 `
 
 export const Shell = styled.div`
-  max-width: 1100px;
+  max-width: 1400px;
   width: 100%;
   height: 100vh;
   padding: 30px;
   box-sizing: border-box;
 
   display: grid;
-  grid-template-columns: 100px 50px repeat(3, 1fr);
+  grid-template-columns: 100px 1fr 250px;
   grid-template-rows: repeat(3, 1fr);
-  grid-gap: 50px;
+  grid-column-gap: 25px;
+  align-items: center;
+  justify-items: center;
 
   > *:first-child {
     grid-column: 1 / 2;
@@ -48,24 +61,38 @@ export const Shell = styled.div`
   }
 
   > *:nth-child(2) {
-    grid-column: 3 / 4;
-    grid-row: 1 / 2;
+    grid-column: 2 / 3;
+    grid-row: 1 / 4;
   }
 
   > *:nth-child(3) {
-    grid-column: 4 / 5;
-    grid-row: 2 / 3;
+    grid-column: 3;
+    grid-row: 1 / 2;
+    align-self: start;
   }
 
   > *:nth-child(4) {
-    grid-column: 5 / 6;
-    grid-row: 3;
+    grid-column: 3;
+    grid-row: 2 / 3;
   }
+
+  > *:nth-child(5) {
+    grid-column: 3;
+    grid-row: 3;
+    align-self: end;
+  }
+`
+
+const Content = styled.div`
+  border: 4px solid black;
+  height: 100%;
+  width: 100%;
 `
 
 class IndexPage extends Component {
   state = {
     platformIsSunken: true,
+    hovered: null,
   }
 
   componentDidMount() {
@@ -77,17 +104,27 @@ class IndexPage extends Component {
       <Page>
         <Shell>
           <Header />
-          <Button>
+          <Content />
+          <Button
+            onMouseEnter={() => this.setState({ hovered: 'work' })}
+            onMouseLeave={() => this.setState({ hovered: false })}
+          >
+            <MeAtWork isAnimating={this.state.hovered === 'work'} />
             <VerticalText size={25} text="Work" />
-            <MeAtWork isAnimating={true} />
           </Button>
-          <Button>
+          <Button
+            onMouseEnter={() => this.setState({ hovered: 'code' })}
+            onMouseLeave={() => this.setState({ hovered: false })}
+          >
+            <ComputerDesktop isAnimating={this.state.hovered === 'code'} />
             <VerticalText size={25} text="Code" />
-            <ComputerDesktop />
           </Button>
-          <Button>
+          <Button
+            onMouseEnter={() => this.setState({ hovered: 'music' })}
+            onMouseLeave={() => this.setState({ hovered: false })}
+          >
+            <Amplifier isAnimating={this.state.hovered === 'music'} />
             <VerticalText size={25} text="Music" />
-            <Amplifier />
           </Button>
         </Shell>
       </Page>
