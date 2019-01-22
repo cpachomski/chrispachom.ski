e386b674a1b5e7bde7f2a193e6080bb6ee7eea23
cpachomski <cpachomski@gmail.com>
2019-01-22 16:33:34 -0500

    add build env to get env vars for google analytics

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 exports.onCreateBabelConfig = ({ stage, actions }, pluginOptions) => {
   const ssr = stage === `build-html` || stage === `build-javascript`
 
